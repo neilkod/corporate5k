996c3922e704ea63d36868f89031e2622825ed57
Neil Kodner <nkodner@gmail.com>
2011-03-28 09:55:47 -0400

    added a few more plot options

--- analyze_times.R
+++ analyze_times.R
@@ -20,8 +20,14 @@ summary(cbsi_runners[cbsi_runners$gender=="Male",]$minutes)
 
 # plot of all finishing times & boxplot of male vs female scores
 p <- ggplot(cbsi_runners,aes(gender,minutes))
- p+geom_boxplot() + geom_jitter() + opts(title="CBS Interactive finishing times for MB Corporate 5k")
+p + geom_boxplot(aes(alpha=.4,fill=gender)) + coord_flip() + geom_point(shape=4,size=6,aes(colour=cbsi_runners$gender)) + opts(title="Finishing times for CBSi 5k participants - 2011 Mercedes-Benz Corporate Challenge, Ft. Lauderdale")
+
+# density plots of male/female finishing times
 
 plot(density(cbsi_runners[cbsi_runners$gender=="Male",]$minutes),main="Male CBSi participant times",sub="2011 Mercedes Benz Corporate Challenge 5k")
 
 plot(density(cbsi_runners[cbsi_runners$gender=="Female",]$minutes),main="Female CBSi participant times",sub="2011 Mercedes Benz Corporate Challenge 5k")
+
+# combined density plot
+m<-ggplot(cbsi_runners,aes(x=minutes))
+m+geom_density(aes(alpha=.65,fill=factor(gender))) + scale_x_continuous(limits =c(10,90)) + opts(title="Distribution of finishing times for CBSi 5k participants")
